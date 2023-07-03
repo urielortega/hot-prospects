d9b2b6eed79be0a351eaa5a8541c88a4a317f691
Uriel Ortega <urielortega2509@gmail.com>
2023-07-03 10:39:12 -0600

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Uriel Ortega on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
